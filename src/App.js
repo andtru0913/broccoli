533b4474e7335fc4d04b8ec8c6bf8d4dda9aa6e7
Annmanf <anna@manfredsson.se>
2022-10-04 14:49:04 +0200

    assets folder with nice images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import logo from './assets/broccoli.png';
+import bg_image from './assets/header.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={bg_image} className="bg-image-header" alt="image-header" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
