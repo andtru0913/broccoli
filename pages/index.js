107062d772c0124857509a3ad053d922655054b3
Annmanf <anna@manfredsson.se>
2022-10-04 16:38:37 +0200

    testing a new page and Links!

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/our_services/first-service">this page!</Link>
         </h1>
 
         <p className={styles.description}>
