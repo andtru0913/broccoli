2b347ee1043cf49febbac7dda079e2ab1ab8f522
andtru0913 <andtru0913@gmail.com>
2022-10-06 08:28:11 +0200

    Revert "Authentication"
    This reverts commit 342b92ff836da1759db2cf903c50ae39b3b075cc.
    
    # Conflicts:
    #	pages/intranet/index.js
    #	prisma/schema.prisma

--- Database.js
+++ Database.js
@@ -9,26 +9,3 @@ export async function createNewUser(username, password, firstname, lastname, add
 
     })
 }
-
-export async function login(input_username, input_password) {
-    const query = await prisma.user.findMany({
-        where: {
-            username: input_username
-        },
-        select: {
-            id: true,
-            password: true,
-            salt: true,
-        }
-    })
-    let salt = query[0].salt
-    let id = query[0].id
-    let account_password = query[0].password
-    const hashedPassword = await bcrypt.hash(input_password, salt)
-    if (hashedPassword === account_password) {
-        return id
-    }
-    else {
-        return null
-    }
-}
