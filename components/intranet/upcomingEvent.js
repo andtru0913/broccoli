2e5f989de06d62659911fc4b498275653a2d410c
elma97 <esati@student.chalmers.se>
2022-11-28 15:06:24 +0100

    första sidan intranät

--- components/intranet/upcomingEvent.js
+++ components/intranet/upcomingEvent.js
@@ -1,7 +1,7 @@
 const upcomingEvent = ({ date, title, description }) => {
 
   return (
-    <div className="bg-secondary cursor-pointer transition-all duration-300 ease-in-out md:hover:scale-x-105 border shadow rounded  my-2">
+    <div className="bg-secondary cursor-pointer transition-all duration-300 ease-in-out md:hover:scale-x-105 bg-secondary-l1/40  my-2">
       <div className=" flex flex-1 m-2 ">
         <div className="flex flex-col ml-2">
           <h4>{title}</h4>
