bf0e7b2fd20f800a2c56d2f9a361549b1cc77ca1
andtru0913 <andtru0913@gmail.com>
2023-02-10 11:01:59 +0100

    editprofile

--- pages/api/editProfile.js
+++ pages/api/editProfile.js
@@ -6,16 +6,24 @@ export default async function handler(req, res) {
     if(req.method !== 'POST') {
         res.redirect(302, '../intranet')
     }
-    let cookies = JSON.parse(req.cookies["token"] || null)
-    if(!!cookies) {
-        if (await verify(cookies) === req.body.id) {
-
-            await editProfile(req.body.id, req.body.username, req.body.password, req.body.email, req.body.address, req.body.privatenumber, req.body.worknumber, req.body.myImage, req.body.description,  new Date(req.body.birthdate))
-                .catch(e => {
-                    console.error(e.message)
-                })
+    try {
+        let cookies = JSON.parse(req.cookies["token"] || null)
+        if(!!cookies) {
+            if (await verify(cookies) === req.body.id) {
+                await editProfile(req.body.id, req.body.username, req.body.password, req.body.email, req.body.address, req.body.privatenumber, req.body.worknumber, req.body.myImage, req.body.description,  new Date(req.body.birthdate))
+                    .catch(e => {
+                        console.error(e.message)
+                    })
+            } else {
+                res.status(403).send("Unauthorized")
+            }
+        } else {
+            res.status(403).send("Token not found")
         }
+    } catch (e) {
+        res.status(500).json({ error: e })
     }
+
 
     res.redirect(302, '../intranet/profile');
 }
