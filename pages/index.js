a321f5231cf2acf72d293f597a6c4791cfff7210
andtru0913 <andtru0913@gmail.com>
2023-02-21 08:08:12 +0100

    build

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ export const getStaticProps = async () => {
   const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`;
   const data = await fetch(url);
   const feed = await data.json();
-
   return {
     props: {
       feed: feed,
