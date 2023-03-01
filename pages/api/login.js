34efe356f6fcf8f2eec8c0f2b95213a73cc054f0
andtru0913 <andtru0913@gmail.com>
2023-03-01 11:45:32 +0100

    login

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,24 +3,23 @@ import {serialize} from 'cookie'
 import {getToken} from "../../tokens";
 
 export default async function handler(req, res) {
-    if(req.method !== 'POST') {
+    if (req.method !== 'POST') {
         if (!!req.body.redirect) {
             res.redirect(302, '../intranet')
         } else {
-            res.status(403).json({error: 'Not a POST request'})
+            res.status(403).json({ error: 'Not a POST request' })
         }
     } else {
         const query = await login(req.body.username, req.body.password)
-        if (!!req.body.redirect) {
-            if (!!query) {
-                res.setHeader('Set-Cookie', serialize('token', JSON.stringify(await getToken(query)), { path: '/' }));
-            }
-            res.redirect(302, '../intranet/')
-        } else {
-            if (!!query) {
+        if (!!query) {
+            res.setHeader('Set-Cookie', serialize('token', JSON.stringify(await getToken(query)), { path: '/' }));
+            if (!!req.body.redirect) {
+                res.redirect(302, '../intranet/')
+            } else {
                 res.status(200).send(await getToken(query))
             }
+        } else {
+            res.status(401).json({ error: 'Wrong username or password' })
         }
-        res.status(401).json({error: 'Wrong username or password'})
     }
 }
