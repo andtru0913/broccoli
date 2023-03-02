c2966b903548be5c393f02bb282d4c931c2d46af
andtru0913 <andtru0913@gmail.com>
2023-03-02 09:54:21 +0100

    events

--- pages/api/admin/createEvent.js
+++ pages/api/admin/createEvent.js
@@ -13,6 +13,12 @@ export default async function handler(req, res) {
       let start = new Date(req.body.start);
       if (req.body.title !== "" && start <= end) {
 
+        await createEvent(
+            req.body.title,
+            req.body.description,
+            start,
+            end
+        );
         const client = new SMTPClient({
           user: "notiser@broccoli.se",
           password: process.env.EMAIL_PASSWORD,
@@ -22,63 +28,55 @@ export default async function handler(req, res) {
         });
         const ics = require("ics");
         ics.createEvent(
-          {
-            start: [
-              start.getFullYear(),
-              (start.getMonth() + 1) % 12,
-              start.getUTCDay(),
-              0,
-              0,
-            ],
-            end: [
-              end.getFullYear(),
-              (start.getMonth() + 1) % 12,
-              end.getUTCDay(),
-              0,
-              0,
-            ],
-            title: req.body.title,
-            description: req.body.description,
-            organizer: { name: "Broccoli", email: "notiser@broccoli.se" },
-          },
-          (err, value) => {
-            if (!err) {
-              client.send(
-                {
-                  text: `${req.body.title}\n${start.toLocaleString("default", {
-                    day: "numeric",
-                    month: "long",
-                  })} - ${end.toLocaleString("default", {
-                    day: "numeric",
-                    month: "long",
-                  })}\n${req.body.description}`,
-                  from: "notiser@broccoli.se",
-                  to: allUsers,
-                  subject: req.body.title,
-                  attachment: [
-                    {
-                      data: value,
-                      name: "event.ics",
-                    },
-                  ],
-                },
-                async (err, _) => {
-                  if (!err) {
-                    await createEvent(
-                      req.body.title,
-                      req.body.description,
-                      start,
-                      end
-                    ).catch((e) => {
-                      throw e;
-                    });
-                  }
+            {
+              start: [
+                start.getFullYear(),
+                (start.getMonth() + 1) % 12,
+                start.getUTCDay(),
+                0,
+                0,
+              ],
+              end: [
+                end.getFullYear(),
+                (start.getMonth() + 1) % 12,
+                end.getUTCDay(),
+                0,
+                0,
+              ],
+              title: req.body.title,
+              description: req.body.description,
+              organizer: { name: "Broccoli", email: "notiser@broccoli.se" },
+            },
+            async (err, value) => {
+              if (!err) {
+                try {
+                  await client.send(
+                      {
+                        text: `${req.body.title}\n${start.toLocaleString("default", {
+                          day: "numeric",
+                          month: "long",
+                        })} - ${end.toLocaleString("default", {
+                          day: "numeric",
+                          month: "long",
+                        })}\n${req.body.description}`,
+                        from: "notiser@broccoli.se",
+                        to: allUsers,
+                        subject: req.body.title,
+                        attachment: [
+                          {
+                            data: value,
+                            name: "event.ics",
+                          },
+                        ],
+                      },
+                  );
+                } catch (e) {
+                  throw e;
                 }
-              );
-            } else {
-              throw err;
+              } else {
+                throw err;
+              }
             }
-          }
         );
       }
     } else {
@@ -91,6 +89,6 @@ export default async function handler(req, res) {
       res.status(200).send("Done!");
     }
   } catch (e) {
-    res.status(500).json({ error: e.message });
+    res.status(500).json({ error: e });
   }
 }
