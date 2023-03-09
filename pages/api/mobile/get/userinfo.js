03f85110304d471967e44ef69852359bf7b5cc83
andtru0913 <andtru0913@gmail.com>
2023-03-09 09:58:03 +0100

    userinfo

--- pages/api/mobile/get/userinfo.js
+++ pages/api/mobile/get/userinfo.js
@@ -6,11 +6,16 @@ export default async function handler(req, res) {
         res.status(403).json({ error: `Not a GET request, received a ${req.method} request` })
     }
     try {
-        const result = JSON.stringify(await getUserinfo(await verify(JSON.parse(req.cookies["token"]))))
+        const token = JSON.parse(req.cookies["token"])
+        const userid = await verify(token)
+        const result = JSON.stringify(await getUserinfo(userid))
         res.status(200).send(result)
     } catch (e) {
-        res.status(500).json({ error: e.message })
+        const token = JSON.parse(req.cookies["token"])
+        const userid = await verify(token)
+        res.status(500).json({ error: e.message, userid: userid, token: token })
     }
+
 }
 
 
