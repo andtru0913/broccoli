92fccfb0306cb06ac5a47a0744c7ac6bbbf4344f
andtru0913 <andtru0913@gmail.com>
2023-03-09 09:37:38 +0100

    userinfo

--- pages/api/mobile/get/userinfo.js
+++ pages/api/mobile/get/userinfo.js
@@ -2,8 +2,8 @@ import {getUserinfo} from "../../../../Database";
 import {verify} from "../../../../tokens";
 
 export default async function handler(req, res) {
-    if(req.method !== 'POST') {
-        res.status(403).json({ error: `Not a POST request, received a ${req.method} request` })
+    if(req.method !== 'GET') {
+        res.status(403).json({ error: `Not a GET request, received a ${req.method} request` })
     }
     try {
         res.status(200).send((JSON.stringify(await getUserinfo(await verify(req.cookies['token'])))))
