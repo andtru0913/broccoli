2c3e1a62301a101e287f7cab038deaa3a986183b
andtru0913 <andtru0913@gmail.com>
2023-03-09 09:36:38 +0100

    userinfo

--- pages/api/mobile/get/userinfo.js
+++ pages/api/mobile/get/userinfo.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
         res.status(403).json({ error: `Not a POST request, received a ${req.method} request` })
     }
     try {
-        res.status(200).send((await getUserinfo(await verify(req.cookies['token']))))
+        res.status(200).send((JSON.stringify(await getUserinfo(await verify(req.cookies['token'])))))
     } catch (e) {
         res.status(500).json({ error: e })
     }
