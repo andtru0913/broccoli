07ee14bffdee61136727395e9c76808f338c4e5b
Annmanf <78025731+Annmanf@users.noreply.github.com>
2023-03-30 16:10:13 +0200

    notific

--- pages/api/mobile/get/userNotifications.js
+++ pages/api/mobile/get/userNotifications.js
@@ -1,20 +1,16 @@
-import {getNotifications} from "../../../../Database";
-import {checkUser, verify} from "../../../../tokens";
+import { getNotifications } from "../../../../Database";
+import { checkUser, verify } from "../../../../tokens";
 
 export default async function handler(req, res) {
-    if(req.method !== 'POST') {
-        res.redirect(302, '../intranet')
+  try {
+    if (await checkUser(req.cookies["token"])) {
+      res
+        .status(200)
+        .send(await getNotifications(verify(req.cookies["token"])));
+    } else {
+      res.status(401).json({ error: "Unauthorized" });
     }
-    try {
-        if (await checkUser(req.cookies['token'])) {
-            res.status(200).send(await getNotifications(verify(req.cookies['token'])))
-        } else {
-            res.status(401).json({ error: 'Unauthorized' })
-        }
-    } catch (e) {
-        res.status(500).json({ error: e })
-    }
-
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 }
-
-
