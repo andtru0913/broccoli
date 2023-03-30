3f0a4421201e3f5bef280f12c1b721f8dcd2f171
Annmanf <78025731+Annmanf@users.noreply.github.com>
2023-03-30 09:55:12 +0200

    join and leave event mobile

---
+++ pages/api/mobile/joinEvent.js
@@ -0,0 +1,25 @@
+import { joinEvent } from "../../Database";
+import { checkUser, verify } from "../../tokens";
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(400).send("not a post");
+  }
+  const [user, user_id] = await Promise.all([
+    JSON.parse(await checkUser(JSON.parse(req.cookies["token"] || null))),
+    verify(JSON.parse(req.cookies["token"])),
+  ]);
+  try {
+    if (!!req.body.eventid) {
+      if (!!user) {
+        await joinEvent(user_id, req.body.eventid);
+        res.status(200).send("Done");
+      } else {
+        res.status(400).send("Unauthorized");
+      }
+    } else {
+      res.status(400).send("Bad request");
+    }
+  } catch {
+    res.status(500).send("Internal server error");
+  }
+}
---
+++ pages/api/mobile/leaveEvent.js
@@ -0,0 +1,30 @@
+import { leaveEvent } from "../../Database";
+import { checkUser, verify } from "../../tokens";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(400).send("Not a POST");
+  }
+  const [user, user_id] = await Promise.all([
+    JSON.parse(await checkUser(JSON.parse(req.cookies["token"] || null))),
+    verify(JSON.parse(req.cookies["token"])),
+  ]);
+  try {
+    if (!!req.body.eventid) {
+      if (!!user) {
+        await leaveEvent(user_id, req.body.eventid);
+      } else {
+        res.status(400).send("Unauthorized");
+      }
+    } else {
+      res.status(400).send("Bad request");
+    }
+  } catch {
+    res.status(500).send("Internal server error");
+  }
+  if (!!req.body.redirect) {
+    res.redirect(302, req.body.redirect);
+  } else {
+    res.status(200).send("Done!");
+  }
+}
