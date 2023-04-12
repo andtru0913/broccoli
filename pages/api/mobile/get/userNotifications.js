2be7ba94dd4106e997c25fc3459da3568086b958
Annmanf <78025731+Annmanf@users.noreply.github.com>
2023-04-12 09:07:47 +0200

    usernotif

--- pages/api/mobile/get/userNotifications.js
+++ pages/api/mobile/get/userNotifications.js
@@ -4,7 +4,9 @@ import { checkUser, verify } from "../../../../tokens";
 export default async function handler(req, res) {
   try {
     if (await checkUser(req.cookies["token"])) {
-      res.status(200).send(verify(req.cookies["token"]));
+      res
+        .status(200)
+        .send(await getNotifications(await verify(req.cookies["token"])));
     } else {
       res.status(401).json({ error: "Unauthorized" });
     }
